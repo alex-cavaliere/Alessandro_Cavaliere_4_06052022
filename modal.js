b9dcb54e77825d3877b01dd9b8a71f98d81ecf65
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2022-05-12 13:33:30 +0200

    debug

--- modal.js
+++ modal.js
@@ -6,13 +6,29 @@ function editNav() {
     x.className = "topnav";
   }
 }
+// function de validation phase de debug
+function validate(){
+  modalBody.addEventListener('submit', function(e){
+    e.preventDefault();
+  })
+  if(formData[0] === "ciao"){
+    alert("ciao anche a te");
+  }else{
+    alert("bho")
+  }
+}
 
 // DOM Elements
 const modalBody = document.querySelector(".modal-body");
+//console.log(modalBody);
 const modalbg = document.querySelector(".bground");
+//console.log(modalbg);
 const modalBtn = document.querySelectorAll(".modal-btn");
+//console.log(modalBtn);
 const formData = document.querySelectorAll(".formData");
+console.log(formData[0]);
 const close_modal = document.querySelectorAll(".close");
+//console.log(close_modal);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
